4eb6fe0e7070c890a6352ed03c56bec5d2600002
Shubham Jain <shubham.jain.1@gmail.com>
2019-01-05 17:47:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Gamepad Remote
+
+> Use your Gamepad Controller to control media on your PC.
+
+Have a wireless controller like this lying around? Use it to remotely control your PC: play/pause, seek, and control volume. 
+
+**Note**: Gamepad remote maps your controller button to keyboard keys. The media actions are not universal but do work on major sites/media players (Netflix, Youtube, VLC et. al). Here's the list of mapping. 
+
+| Gamepad Button  | Media Action | Keyboard key mapping  |
+|---------------|------------|---------------|
+| A  | Play/pause   | Spacebar   |
+| Right/Left Arrow  | Seek   | Right/Left Arrow  |
+|  Up/Down Arrow |  Vol Control  |  Up/Down Arrow |
+
+## Usage
+
+Use to install the package.
+
+`npm install gamepad-remote`
+
+Start the program by:
+
+`gamepad-remote start`
+
+Stop it by:
+
+`gamepad-remote stop`
+
+## License 
+MIT
---
+++ index.js
@@ -0,0 +1,49 @@
+const gamepad = require("gamepad");
+const robot = require("robotjs");
+const startStopDaemon = require("start-stop-daemon");
+
+const GAMEPAD_KEYS = {
+  UP_ARROW: 5,
+  DOWN_ARROW: 6,
+  LEFT_ARROW: 7,
+  RIGHT_ARROW: 8,
+
+  START: 9,
+  BACK: 10,
+
+  A: 16,
+  B: 17,
+  X: 18,
+  Y: 19
+};
+
+startStopDaemon (function () {
+  gamepad.init()
+ 
+  // Create a game loop and poll for events
+  setInterval (gamepad.processEvents, 16);
+  
+  // Scan for new gamepads as a slower rate
+  setInterval (gamepad.detectDevices, 500);
+  
+  gamepad.on ("up", function (id, num) {
+    if (num === GAMEPAD_KEYS.RIGHT_ARROW) {
+      robot.keyTap ("right");
+    }
+  
+    if (num === GAMEPAD_KEYS.LEFT_ARROW) {
+      robot.keyTap ("left");
+    }
+  
+    if (num === GAMEPAD_KEYS.UP_ARROW) {
+      robot.keyTap ("up");
+    }
+  
+    if (num === GAMEPAD_KEYS.DOWN_ARROW) {
+      robot.keyTap ("down");
+    }
+    if (num === GAMEPAD_KEYS.A) {
+      robot.keyTap ("space");
+    }
+  });  
+});
